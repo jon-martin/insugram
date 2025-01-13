8893531a62421ae8853eb13af7a458423464e87e
Jon <jon.martin.sigvaldsen@gmail.com>
2025-01-13 23:29:07 +0100

    Moved commets to meals-table

--- tables.sql
+++ tables.sql
@@ -12,23 +12,18 @@ CREATE TABLE IF NOT EXISTS meal_item (
 	FOREIGN KEY (item_id) REFERENCES items(id),
 	FOREIGN KEY (meal_id) REFERENCES meals(id));
 
-CREATE TABLE IF NOT EXISTS meals ( 
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	insulin REAL,
-	log_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
-
--- hit is a number from 1-5
+-- rate is a number from 1-5
 -- 1 - way too much
 -- 2 - too much
 -- 3 - good
 -- 4 - too little
 -- 5 - way too little
-CREATE TABLE IF NOT EXISTS comments ( 
+CREATE TABLE IF NOT EXISTS meals ( 
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	meal_id INTEGER,
-	hit INTEGER,
-	note TEXT,
-	FOREIGN KEY (meal_id) REFERENCES meals(id));
+	log_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+	insulin REAL,
+	rate INTEGER,
+	note TEXT);
 
 -- TODO: Fill in items
 INSERT INTO items (name, usual_weight, carbs_per_100) VALUES ( 'Ris', 70, 28 );
@@ -36,4 +31,3 @@ INSERT INTO items (name, usual_weight, carbs_per_100) VALUES ( 'Ris', 70, 28 );
 -- To be filled in through API
 INSERT INTO meals (insulin) VALUES ( 20 );
 INSERT INTO meal_item VALUES ( 1, 1, 70 );
-INSERT INTO comments (meal_id, hit, note) VALUES ( 1, 5, 'way too little..');
